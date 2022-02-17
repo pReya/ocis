91a6a3e5aeb0c6d2b26c19e0129b0219fe8de291
Willy Kloucek <wkloucek@owncloud.com>
2022-02-17 10:12:28 +0100

    dev env

--- deployments/examples/ocis_wopi/config/ocis/entrypoint-override.sh
+++ deployments/examples/ocis_wopi/config/ocis/entrypoint-override.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-ocis server&
-sleep 10
+apk add socat
 
-echo "##################################################"
-echo "change default secrets:"
-
-# IDP
-IDP_USER_UUID=$(ocis accounts list | grep "| Kopano IDP " | egrep '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}' -o)
-echo "  IDP user UUID: $IDP_USER_UUID"
-ocis accounts update --password $IDP_LDAP_BIND_PASSWORD $IDP_USER_UUID
-
-# REVA
-REVA_USER_UUID=$(ocis accounts list | grep " | Reva Inter " | egrep '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}' -o)
-echo "  Reva user UUID: $REVA_USER_UUID"
-ocis accounts update --password $STORAGE_LDAP_BIND_PASSWORD $REVA_USER_UUID
-
-echo "default secrets changed"
-echo "##################################################"
-
-wait # wait for oCIS to exit
+socat tcp-l:9200,fork,reuseaddr tcp:172.17.0.1:9200
--- deployments/examples/ocis_wopi/config/wopiserver/entrypoint-override.sh
+++ deployments/examples/ocis_wopi/config/wopiserver/entrypoint-override.sh
@@ -1,18 +1,6 @@
 #/bin/sh!
 set -e
 
-echo "${WOPISECRET}" > /etc/wopi/wopisecret
-echo "${IOPSECRET}" > /etc/wopi/iopsecret
-mkdir -p /var/run/secrets
-echo "$CODIMDSECRET" > /var/run/secrets/codimd_apikey
+apk add socat
 
-cp /etc/wopi/wopiserver.conf.dist /etc/wopi/wopiserver.conf
-sed -i 's/ocis.owncloud.test/'${OCIS_DOMAIN}'/g' /etc/wopi/wopiserver.conf
-sed -i 's/collabora.owncloud.test/'${COLLABORA_DOMAIN}'/g' /etc/wopi/wopiserver.conf
-sed -i 's/wopiserver.owncloud.test/'${WOPISERVER_DOMAIN}'/g' /etc/wopi/wopiserver.conf
-
-touch /var/log/wopi/wopiserver.log
-
-/app/wopiserver.py &
-
-tail -f /var/log/wopi/wopiserver.log
+socat tcp-l:8880,fork,reuseaddr tcp:172.17.0.1:5000
